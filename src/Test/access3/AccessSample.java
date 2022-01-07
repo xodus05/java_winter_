f69c4d8708b7a12a23690eb99c9241fc3b23e66d
Taeyeon <Taeyeon@192.168.0.115>
2022-01-07 15:17:38 +0900

    AccessSample n2 setter getter

---
+++ src/Test/access3/AccessSample.java
@@ -0,0 +1,15 @@
+package Test.access3;
+
+public class AccessSample {
+	public int n1;
+	private int n2;
+	
+	public int getN2() {
+		return n2;
+	}
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+	
+	
+}
