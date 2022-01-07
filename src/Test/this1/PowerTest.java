a7289a61a187e2f77ee87d6868560ce756bb0f42
Taeyeon <Taeyeon@192.168.0.115>
2022-01-07 15:39:58 +0900

    PowerTest

---
+++ src/Test/this1/PowerTest.java
@@ -0,0 +1,20 @@
+package Test.this1;
+
+public class PowerTest {
+	private int n;
+	
+	public void power(int n) {
+		this.n = n * n;
+		System.out.printf("%dÀÇ 2Á¦°ö½ÂÀº %dÀÔ´Ï´Ù.\n", n, this.n);
+	}
+	
+	public int power(int x, int y) {
+		n = 1;
+		
+		for (int i = 0; i < y; i++) {
+			n *= x;
+		}
+		
+		return n;
+	}
+}
